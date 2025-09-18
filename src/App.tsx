c90aa89556b9a33833832bcf5275521e5b85f056
EJlearner <rzrshrp@gmail.com>
2025-09-17 21:07:36 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,43 @@ import {useRef, useState} from 'react';
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
 import topPicture from './assets/images/temp-top-picture.jpg';
+import {FaCheckCircle, FaLock} from 'react-icons/fa';
 
 function getDonationWithFeesAmount(dollars: number): number {
-  // return the amount with 10% fees rounded to the nearest dollar
+  // TODO: confirm fee percentage with payment processor
   return Math.round(dollars * 1.1);
 }
 
+function formatDonation(inputAmount: string) {
+  const numbersAndPeriodsOnly = inputAmount.replace(/[^0-9.]/g, '');
+
+  const [beforePeriod, afterPeriod] = numbersAndPeriodsOnly.split('.');
+
+  const hasDot = numbersAndPeriodsOnly.includes('.');
+  const everyNumberAfterDotIsZero = afterPeriod
+    ?.split('')
+    .every((char) => char === '0');
+
+  let donationNumber;
+
+  if (!hasDot) {
+    donationNumber = beforePeriod;
+  } else if (everyNumberAfterDotIsZero) {
+    donationNumber = beforePeriod;
+  } else {
+    donationNumber = Number(numbersAndPeriodsOnly).toFixed(2);
+  }
+
+  return donationNumber;
+}
+
+function unformatDonation(inputAmount: string) {
+  return Number(inputAmount.replace(/\$/, ''));
+}
+
 function Home() {
-  const [donationDollars, setDonationDollars] = useState(50);
+  const [donationAmount, setDonationAmount] = useState(50);
+  const [inputTextAmount, setInputTextAmount] = useState('50');
   const dialogRef = useRef<HTMLDialogElement | null>(null);
 
   return (
@@ -51,37 +80,59 @@ function Home() {
                   <button
                     key={amount}
                     className={
-                      donationDollars === amount ? 'current' : 'not-current'
+                      amount === donationAmount ? 'current' : 'not-current'
                     }
-                    onClick={() => setDonationDollars(amount)}
+                    onClick={() => {
+                      setDonationAmount(amount);
+                      setInputTextAmount(String(amount));
+                    }}
                   >
                     ${amount}
                   </button>
                 ))}
               </div>
               <div>
-                {/* TODO: Next task add USD shaded text on the right, make font size larger */}
-                <input
-                  type="text"
-                  onChange={(e) => setDonationDollars(Number(e.target.value))}
-                  placeholder="Other Amount"
-                  value={donationDollars}
-                />
+                <div className="text-wrapper">
+                  <span className="dollar-symbol">$</span>
+                  <span className="currency-type">USD</span>
+                  <input
+                    className="dollar-text-input"
+                    type="text"
+                    onChange={(e) => setInputTextAmount(e.target.value)}
+                    onBlur={(e) => {
+                      const formattedAmount = formatDonation(e.target.value);
+                      const unformattedAmount =
+                        unformatDonation(formattedAmount);
+                      setInputTextAmount(formattedAmount);
+                      setDonationAmount(unformattedAmount);
+                    }}
+                    placeholder="Other Amount"
+                    value={inputTextAmount}
+                  />
+                </div>
               </div>
-              <div>
-                Cover processing fees for my {donationDollars} by increasing it
-                to {getDonationWithFeesAmount(donationDollars)}
+              <div className="increase-blurb-wrapper">
+                <FaCheckCircle className="increase-icon" />
+                <p>
+                  Increase my donation to{' '}
+                  <strong>${getDonationWithFeesAmount(donationAmount)}</strong>{' '}
+                  so that processing fees are covered.
+                </p>
               </div>
-              <button>Enter payment info</button>
+              <button className="submit-button">
+                <FaLock />
+                <span>Enter payment info</span>
+              </button>
             </div>
           </div>
-          <p>
-            If you have questions about giving or would like to learn more about
-            our programs, please contact us.
-          </p>
         </div>
+        <p className="questions-text">
+          {/* TODO: Contact us should be link */}
+          If you have questions about giving or would like to learn more about
+          our programs, please contact us.
+        </p>
       </section>
-      {/* Quotes section */}
+      {/* TODO: Quotes section */}
       <section />
       <section>
         <h2>
